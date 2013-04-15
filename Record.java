7296faf4cc6f10de792d24957fa37b8487a1c38d
Sean Yeh <sean.yeh@gmx.com>
2013-04-14 23:33:25 -0400

    Add some Record things

--- Record.java
+++ Record.java
@@ -13,7 +13,11 @@ public class Record implements Comparable{
     public int content;
 
     // L and R should not be Records...figure this out later
-    public Record(int n, double p, boolean b, double c, Record l, Record r){
+    /* public Record(int n, double p, boolean b, double c, Record l, Record r){ */
+
+    // I think L and R are indexes to the left/right children in the A array?
+    //  and this record should be the combined plan of L && R...?
+    public Record(int n, double p, boolean b, double c, int l, int r){
         num = n;
         selectivity = p;
         isNoBranch = b;
@@ -29,7 +33,8 @@ public class Record implements Comparable{
 
     public String toString(){
         return "Record: n = " + num + " p = " + selectivity +
-            " b = " + isNoBranch + " content = " + content;
+            " b = " + isNoBranch + " content = " + content +
+            " cost = " + cost;
     }
 
     // For sorting in increasing order: sorted by num (number of
